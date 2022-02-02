8525e5bcb6ec75c3eb5fde5cdece39ae6bc9ae57
yudai0731 <yudai40018@gmail.com>
2022-02-02 12:15:06 +0000

    complete ~P40

---
+++ ch2/missingValue.R
@@ -0,0 +1,25 @@
+# load package
+require(zoo)
+require(data.table)
+
+# load data
+unemp <- fread("UNRATE.csv")
+# translate column "DATE" to datetime object
+unemp[, DATE :=as.Date(DATE)]
+setkey(unemp,DATE)
+
+# データが無作為に欠損しているデータセットを生成
+rand.unemp.idx <- sample(1:nrow(unemp), .1*nrow(unemp))
+rand.unemp <- unemp[-rand.unemp.idx]
+
+# 失業率の高い月にデータが欠損値している可能性の高いデータセットを生成
+high.unemp.idx <- which(unemp$UNRATE>8)
+num.to.select <- .2*length(high.unemp.idx)
+high.unemp.idx <- sample(high.unemp.idx,)
+bias.unemp <- unemp[-high.unemp.idx]
+
+# ローリング結合
+all.dates <-seq(from = unemp$DATE[1],to = tail(unemp$DATE,1),by="months")
+rand.unemp = rand.unemp[J(all.dates),roll=0]
+bias.unemp = bias.unemp[J(all.dates),roll=0]
+rand.unemp[,rpt :=is.na(UNRATE)]
